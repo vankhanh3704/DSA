ca2a1c8f04883f44160965a560625d478b458c3c
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-03-18 09:39:26 +0700

    first

--- Sàng số nguyên tố/main 4.hpp
+++ Sàng số nguyên tố/main 4.hpp
@@ -1,12 +1,9 @@
 #include <iostream>
-
 using namespace std;
-int main(){
-    int n;
+
+int main()
+{
+    int n ;
     cin >> n;
     cout << n;
-    for(int i = 0; i < n; i++){
-        
-    }
 }
-
--- main.hpp
+++ main.hpp
@@ -1,12 +1,8 @@
 #include <iostream>
-
 using namespace std;
+
 int main(){
     int n;
     cin >> n;
     cout << n;
-    for(int i = 0; i < n; i++){
-        
-    }
 }
-
